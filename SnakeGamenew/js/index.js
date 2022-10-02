6be5bf40f18093074a9693e84c3eb057f9f2751c
Varunjoshi22 <92745621+Varunjoshi22@users.noreply.github.com>
2022-10-02 14:06:32 +0530

    Updated Index.js
    Changed the logic for moving snake in a ordered way.

--- SnakeGamenew/js/index.js
+++ SnakeGamenew/js/index.js
@@ -68,6 +68,10 @@ function gameEngine() {
 	}
 
 	//Movign the Snake
+	for (let i = snakeArr.length - 2; i >= 0; i--) {
+		// -2 means we are taking the  second last element of the snake
+		snakeArr[i + 1] = { ...snakeArr[i] };
+	}
 
 
 	snakeArr[0].x += inputDir.x;
